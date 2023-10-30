ed6a85df209d6348616ae6518406012dbc81adb2
garvitnandwana <nandwanagarvit4@gmail.com>
2023-10-30 18:10:09 +0530

    Added links, only english news , adding different topics and only 20 articles

--- News_Every_Morning/main.py
+++ News_Every_Morning/main.py
@@ -1,17 +1,27 @@
 import requests
 from emails import send_emails
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-30&sortBy=publishedAt&apiKey=f01fddc8fb5c49eb8455549965f97e66"
+# For adding the topic that the user needs
+topic = 'Tesla'
+
+url = """https://newsapi.org/v2/everything?q={topic}&
+from=2023-09-30&sortBy=publishedAt&apiKey=f01fddc8fb5c49eb8455549965f97e66&language=en"""
+
 api_key = "f01fddc8fb5c49eb8455549965f97e66"
 
+# Make Request
 request = requests.get(url)
 
+# Get a dictionary with the required data
 response = request.json()
 
-# Important
+# Accessing the article title and description
 body = ""
-for article in response['articles']:
-    body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+# Limiting the articles to a maximum of 20
+for article in response['articles'][:20]:
+    body = body + article['title'] \
+          + '\n' + article['description'] \
+          + '\n' + article['url'] + 2 * '\n' 
 
 body = body.encode('utf8')
 send_email = send_emails(body)
